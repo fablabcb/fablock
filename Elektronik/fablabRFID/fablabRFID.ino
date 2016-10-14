0bd14f473cb5c4c609c848c9bcbc4aaa74f1d8f8
Nanu Frechen <nanu@fablab-cottbus.de>
2016-10-14 13:22:59 +0200

    Hiermit läuft der Motor jetzt

--- Elektronik/fablabRFID/fablabRFID.ino
+++ Elektronik/fablabRFID/fablabRFID.ino
@@ -2,8 +2,8 @@
 #include "A4988.h"
 
 // using a 200-step motor (most common)
-// pins used are DIR, STEP, MS1, MS2, MS3 in that order
-A4988 stepper(200, 8, 9, 10, 11, 12);
+// pins used are steps, DIR, STEP_pin, enable, MS1, MS2, MS3 in that order
+A4988 stepper(200, 8, 9, 13, 10, 11, 12);
 const int Locked = 5; // Endstop for the locked position
 const int Unlocked = 4; // Endstop for the unlocked position
 const int Closed = 7; // Endstop for detecting whether the window 
@@ -14,7 +14,7 @@ int d = -1; // direction of motor rotation
 void setup() {
   stepper.disable();
   Serial.begin(9600);
-  stepper.setRPM(150);
+  stepper.setRPM(120);
   // Set full speed mode (microstepping also works for smoother hand movement
   stepper.setMicrostep(1);
   pinMode(Closed, INPUT_PULLUP);
@@ -32,18 +32,22 @@ void setup() {
   while(digitalRead(Unlocked) == LOW){
     Serial.println("Opening Window");
     // Open window locking mechanism
-    stepper.rotate(-10);
+    stepper.enable();
+    stepper.move(-100);
+    stepper.disable();
   }//ends when EndStopUnlocked is pressed
-  Serial.println("Open reached");
+  //Serial.println("Open reached");
 
   delay(10000);
-  
+     
   while(digitalRead(Locked) == LOW){
     // wait for WindowClosed Endstop to be pressed    
     if(digitalRead(Closed) == HIGH){
       Serial.println("closing");
       // Close window locking mechanism
-      stepper.rotate(10);
+      stepper.enable();
+      stepper.move(100);
+      stepper.disable();
     }else{
       Serial.println("waiting to close");
     }
@@ -54,7 +58,6 @@ void setup() {
 //    }
   }  //don't stop cycle until window is
   //locked
-  stepper.disable();
   Serial.println("window closed");  
 }
 
